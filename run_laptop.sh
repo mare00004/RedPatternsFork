85c2684687b1769b6eb22845150315e0ccfe4131
Maximilian Reinhard <X3ndor@users.noreply.github.com>
2025-11-09 00:45:53 +0100

    Test run script for laptop

---
+++ run_laptop.sh
@@ -0,0 +1,10 @@
+build/red-patterns --use-convolution \
+    --T=1 \
+    --DT=5.0e-03 \
+    --NO=1 \
+    --U=111.15e-18 \
+    --PSI=0.02 \
+    --gamma=1.8e-10 \
+    --delta=1e-11 \
+    --kappa=0.0 \
+    --out-dir=data
