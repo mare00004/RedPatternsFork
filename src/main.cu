83624c43a882308939f69f60c987d9870b0de307
Maximilian Reinhard <X3ndor@users.noreply.github.com>
2025-11-09 00:48:13 +0100

    Using new argument parsing

--- src/main.cu
+++ src/main.cu
@@ -1,6 +1,7 @@
-#include "cli_arguments_parser.cuh"
-#include "config.h";
+#include "cli.cuh"
+#include "config.h"
 #include "cuda_utils.cuh"
+#include "parameters.cuh"
 #include "simulations.cuh"
 #include <stdio.h>
 
@@ -8,6 +9,20 @@
 int main(int argc, char *argv[]) {
     SimConfig cfg;
     setDefaults(&cfg);
+    if (parseArguments(argc, argv, &cfg)) {
+        return EXIT_FAILURE;
+    }
+    // Overwrite constants (like in old readParameters)
+    U = cfg.model.U;
+    PSI = cfg.model.PSI;
+    IT = cfg.run.DT;
+    T = cfg.run.T;
+    NO = cfg.run.NO;
+    h_gamma = cfg.model.gamma;
+    h_delta = cfg.model.delta;
+    h_kappa = cfg.model.kappa;
+    NT = ceil(T / IT);
+
     // detect cuda device
     cudaDeviceProp prop;
     checkCuda(cudaGetDeviceProperties(&prop, 0));
@@ -15,9 +30,8 @@ int main(int argc, char *argv[]) {
     checkCuda(cudaChooseDevice(&cudaDevice, &prop));
     printf("\nDevice Name: %s\n", prop.name);
     printf("Compute Capability: %d.%d\n\n", prop.major, prop.minor);
-    // read parameter arguments from command line
-    readParameters(argc, argv);
+
     // run simulation
     runSim(&cfg);
-    return 0;
+    return EXIT_SUCCESS;
 }
